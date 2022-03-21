6ebd9527d88a827cde2336487e380ac602ff520a
Anos176 <pm@moonivia.com>
2022-03-21 16:06:39 -0400

    idk

--- src/FindPath.java
+++ src/FindPath.java
@@ -1,2 +1,27 @@
 public class FindPath {
+
+    Map piramidMap;
+
+    public FindPath(String fileName) {
+
+    }
+
+    public DLStack path() {
+
+    }
+
+    public Map getMap() {
+
+    }
+
+    public boolean isDim(Chamber currentChamber) {
+
+    }
+
+    public Chamber bestChamber (Chamber currentChamber) {
+
+    }
+
+
+
 }
